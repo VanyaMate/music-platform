f7e966ff5ec49e97fdfdca2e44d21ab4ada8569e
VanyaMate <buhu653@mail.ru>
2023-03-20 18:34:50 +0300

    ustal

---
+++ client/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/global.scss';
+
+// @ts-ignore
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import Navbar from "../components/Navbar";
+
+const Index = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Button>Button</Button>
+        </div>
+    );
+};
+
+export default Index;
